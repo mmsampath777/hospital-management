7131a27b6b2097219046ea48576e2d050c9969a4
mmsampath777 <mmsampathram@gmail.com>
2025-08-05 19:00:28 +0530

    Added new sections in home page

--- js/splash.js
+++ js/splash.js
@@ -1,10 +1,13 @@
 window.addEventListener("load", () => {
   const splash = document.getElementById("splash-screen");
   const main = document.getElementById("main-content");
+  const footer = document.getElementById("footer");
 
   setTimeout(() => {
     splash.style.display = "none";
     main.style.display = "block";
+    footer.style.display = "block";
     main.classList.add("fadein");
+    footer.classList.add("fadein");
   }, 2500);
 });
